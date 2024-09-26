bf76fb6b86cda7b1a05a2be0de5eb89871b95268
119110342 DANIEL DANTAS FONSECA <daniel.fonseca@lcc3-098.lcc.ufcg.edu.br>
2024-09-26 16:48:45 -0300

    corrige erros de atributos. implementa run().

--- java/base/TaskProducer.java
+++ java/base/TaskProducer.java
@@ -3,8 +3,8 @@ import java.util.concurrent.BlockingQueue;
 public class TaskProducer implements Runnable {
 
     private final BlockingQueue<Task> taskQueue;
-    private int idProdutor;
-    private final long idTask = 0;
+    private final int idProdutor;
+    private long idTask = 0;
 
     public TaskProducer(BlockingQueue<Task> taskQueue, int idProdutor) {
         this.taskQueue = taskQueue;
@@ -13,7 +13,15 @@ public class TaskProducer implements Runnable {
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        while (true) {
+            Task task = new Task(idTask++);
+            try {
+                taskQueue.put(task);
+                System.out.println("Produtor " + idProdutor + " produziu a task de id " + task.getId());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Produtor " + idProdutor + "foi interrompido");
+            }
+        }
     }
 }
