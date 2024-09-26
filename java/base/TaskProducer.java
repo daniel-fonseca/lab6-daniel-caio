1f2643759f2ea4c42aa780656b2f7005f6f7eb75
119110342 DANIEL DANTAS FONSECA <daniel.fonseca@lcc3-098.lcc.ufcg.edu.br>
2024-09-26 16:40:58 -0300

    cria inicio de taskproducer

--- java/base/TaskProducer.java
+++ java/base/TaskProducer.java
@@ -1,4 +1,15 @@
+import java.util.concurrent.BlockingQueue;
+
 public class TaskProducer implements Runnable {
+
+    private final BlockingQueue<Task> taskQueue;
+    private int idProdutor;
+    private long idTask = 0;
+
+    public TaskProducer(BlockingQueue<Task> taskQueue, int idProdutor) {
+        taskQueue = this.taskQueue;
+        idProdutor = this.idProdutor;
+    }
 
     @Override
     public void run() {
