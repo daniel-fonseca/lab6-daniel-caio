1fc50451421a08e82b960b8204218f924258f06c
CaioVFA <caio.araujo@ccc.ufcg.edu.br>
2024-09-26 17:53:00 -0300

    Desfazendo as mudanças do cenario base

--- java/base/Task.java
+++ java/base/Task.java
@@ -1,28 +1,19 @@
 import java.util.Random;
 
 public class Task {
-    private final long id;
-    private final long creationTime;
+    long id;
 
     public Task(long id) {
         this.id = id;
-        this.creationTime = System.currentTimeMillis();
-    }
-
-    public long getId(){
-        return this.id;
-    }
-
-    public long getCreationTime() {
-        return this.creationTime;
     }
 
     public void execute() {
         try {
+            // generating a number between 1000 and 15000
             long execDuration = 1000 + (long) (new Random().nextFloat() * (15000 - 1000));
             Thread.sleep(execDuration);
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            e.printStackTrace();
         }
     }
 }
--- java/base/TaskProducer.java
+++ java/base/TaskProducer.java
@@ -1,29 +1,8 @@
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicLong;
-
 public class TaskProducer implements Runnable {
-    private final BlockingQueue<Task> taskQueue;
-    private final int idProdutor;
-    private static final AtomicLong idTaskGenerator = new AtomicLong(0); // Gerador de IDs únicos
-
-    public TaskProducer(BlockingQueue<Task> taskQueue, int idProdutor) {
-        this.taskQueue = taskQueue;
-        this.idProdutor = idProdutor;
-    }
 
     @Override
     public void run() {
-        try {
-            while (true) {
-                long idTask = idTaskGenerator.getAndIncrement(); 
-                Task task = new Task(idTask);
-                taskQueue.put(task);
-                System.out.println("Produtor " + idProdutor + " produziu a tarefa de id " + task.getId());
-                Thread.sleep(5000);
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.out.println("Produtor " + idProdutor + " foi interrompido.");
-        }
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
