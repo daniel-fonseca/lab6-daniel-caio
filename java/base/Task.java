6c8abab130dbf90bc350db2e18b401182bb54ead
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-09-26 17:21:43 -0300

    corrige task e adiciona funcionalidades para parar de ficar serial

--- java/base/Task.java
+++ java/base/Task.java
@@ -1,23 +1,28 @@
 import java.util.Random;
 
 public class Task {
-    long id;
+    private final long id;
+    private final long creationTime;
 
     public Task(long id) {
         this.id = id;
+        this.creationTime = System.currentTimeMillis();
     }
 
     public long getId(){
         return this.id;
     }
 
+    public long getCreationTime() {
+        return this.creationTime;
+    }
+
     public void execute() {
         try {
-            // generating a number between 1000 and 15000
             long execDuration = 1000 + (long) (new Random().nextFloat() * (15000 - 1000));
             Thread.sleep(execDuration);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
